7fb2e7233724f567bee65aa9c77bb174ca67ca37
adithyavinod2300-hub <adithyavinod2300@gmail.com>
2026-05-12 19:19:50 +0530

    Fix gitignore and clean unwanted files

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from google import genai
+import os
+from dotenv import load_dotenv
+load_dotenv()
 app = FastAPI()
 
-# allow frontend connection
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,14 +14,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+
 @app.get("/")
 def home():
     return {"message": "Backend is working"}
 
+
 @app.post("/chat")
 async def chat(data: dict):
-    user_msg = data.get("message")
+    try:
+        user_msg = data.get("message")
 
-    return {
-        "response": f"You said: {user_msg}"
-    }
+        response = client.models.generate_content(
+            model="gemini-3.1-flash-lite",   # ✅ updated model name
+            contents=user_msg,
+        )
+
+        return {
+            "response": response.text
+        }
+
+    except Exception as e:
+        print("ERROR:", e)
+        return {
+            "response": "AI error occurred. Check backend terminal."
+        }
